d9b9515a41927ed8f2bf1bd9eb13d70566a018f2
NikodemJanaszak <nikodem.janaszak@gmail.com>
2019-03-13 16:42:04 +0100

    sample working phpMyAdmin client on Java

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+import java.sql.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try{
+            //Class.forName("com.mysql.jdbc.Driver");
+            Connection con=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/planner?useLegacyDatetimeCode=false&serverTimezone=UTC", "admin", "adminadmin");
+            Statement stmt=con.createStatement();
+            System.out.println("Połączono");
+            ResultSet rs=stmt.executeQuery("select * from plan");
+            while(rs.next())
+                System.out.println(rs.getInt(1)+"  "+rs.getString(2));
+            con.close();
+        }catch(Exception e){ System.out.println(e);}
+    }
+}
